514e0840490e7dac005296f9b584dff94ad08272
Mark Polkhovskiy <polkhovskiymark@gmail.com>
2022-11-15 10:57:55 -0500

    Added opencv to the project for android platform

--- android/app/src/main/kotlin/com/smartshot/smart_shot/MainActivity.kt
+++ android/app/src/main/kotlin/com/smartshot/smart_shot/MainActivity.kt
@@ -12,6 +12,7 @@ import android.content.IntentFilter
 import android.os.BatteryManager
 import android.os.Build.VERSION
 import android.os.Build.VERSION_CODES
+import org.opencv.android.OpenCVLoader
 
 
 class MainActivity: FlutterActivity() {
@@ -26,11 +27,19 @@ class MainActivity: FlutterActivity() {
         val batteryLevel = getBatteryLevel()
 
         if (batteryLevel != -1) {
-          result.success(batteryLevel)
-        } else {
+
+          if (OpenCVLoader.initDebug()) {
+            result.success(110)
+          }
+          else {
+            result.success(batteryLevel)
+          }
+        }
+        else {
           result.error("UNAVAILABLE", "Battery level not available.", null)
         }
-      } else {
+      }
+      else {
         result.notImplemented()
       }
     }
@@ -41,7 +50,8 @@ class MainActivity: FlutterActivity() {
     if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
       val batteryManager = getSystemService(Context.BATTERY_SERVICE) as BatteryManager
       batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
-    } else {
+    }
+    else {
       val intent = ContextWrapper(applicationContext).registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
       batteryLevel = intent!!.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100 / intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
     }
