4f0a8b13c660f234e45c211af6cb6d4a76041438
Mark Polkhovskiy <polkhovskiymark@gmail.com>
2022-11-22 14:02:45 -0500

    Added the haar cascade to android init sequence

--- android/app/src/main/kotlin/com/smartshot/smart_shot/MainActivity.kt
+++ android/app/src/main/kotlin/com/smartshot/smart_shot/MainActivity.kt
@@ -1,26 +1,46 @@
 package com.smartshot.smart_shot
 
-import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 import org.opencv.android.OpenCVLoader
-import org.opencv.core.Mat
-import org.opencv.imgcodecs.Imgcodecs.imread
 import org.opencv.objdetect.CascadeClassifier
+import java.io.File
+import java.io.FileOutputStream
 
 
 class MainActivity: FlutterActivity() {
   private val _channel = "smartshot/opencv";
   private var _opencvLoaded = false;
-  var faceDetector: CascadeClassifier? = null;
+  private lateinit var haarFace: CascadeClassifier;
 
   override fun onFlutterUiDisplayed() {
     super.onFlutterUiDisplayed()
     if (!_opencvLoaded && OpenCVLoader.initDebug()) {
-      _opencvLoaded = true;
+//      val haarUri: Uri = Uri.parse("android.resource://com.shartshot.smart_shot/" + R.drawable.haarcascade_frontalface_alt2);
+//      haarFacePath = haarUri.path.toString();
+      val `is` = resources.openRawResource(R.raw.haarcascade_frontalface_alt2);
+      val cascadeDir = getDir("cascade", MODE_PRIVATE);
+      val caseFile = File(cascadeDir, "haarcascade_frontalface_alt2.xml");
+
+      val fos: FileOutputStream = FileOutputStream(caseFile);
+
+      val buffer = ByteArray(4096);
+      var bytesRead: Int;
+
+      while (`is`.read(buffer).also { bytesRead = it } != -1) {
+        fos.write(buffer, 0, bytesRead);
+      }
+      `is`.close();
+      fos.close();
+
+      haarFace = CascadeClassifier(caseFile.absolutePath);
+
+      if(!haarFace.empty()){
+        cascadeDir.delete();
+        _opencvLoaded = true;
+      }
     }
   }
 
@@ -33,7 +53,13 @@ class MainActivity: FlutterActivity() {
           if (call.hasArgument("path")) {
             val path = call.argument<String>("path");
 //            var image: Mat = imread(path);
-            result.success("$path/success");
+//            if (!image.empty()) {
+
+              result.success("Success");
+//            }
+//            else {
+//              result.error("IMAGE ERROR", "Was not able to load image", null);
+//            }
           }
           else {
             result.error("ARGUMENT ERROR", "Path not provided.", null);
